64ecac1cbac16f1ab3e4ad72995c8129d028b817
jsnelgro <johnny.snelgrove@gmail.com>
2023-12-28 20:38:48 -0800

    break out some code and add users feature

---
+++ plots.js
@@ -0,0 +1,85 @@
+import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
+
+export const heatmapPlot = (heatmapView, calcProblemStats, setHighlightedProblem) => {
+    const computeColorOptions = () => {
+        switch (heatmapView) {
+            case "correctPercent":
+                return { scheme: "RdYlGn", reverse: false, pivot: 75 }
+            case "responseTime":
+                return { scheme: "RdYlGn", reverse: true, pivot: 10 }
+            default:
+                return { scheme: "rdylbu", reverse: false }
+        }
+    }
+
+    const computeText = (d) => {
+        switch (heatmapView) {
+            case "correctPercent":
+                return d.correctPercent ? d.correctPercent.toFixed(1) + "%" : undefined
+            case "responseTime":
+                return d.responseTime ? d.responseTime + "s" : undefined
+            default:
+                return d[heatmapView] ?? undefined
+        }
+    }
+    const problems = Array(10).fill(0)
+        .flatMap((_, i) => Array(10).fill(0).map((_, j) => ({ a: i + 1, b: j + 1 })))
+        .map(it => {
+            const stats = calcProblemStats(`${it.a}*${it.b}`)
+            return { ...it, ...stats }
+        })
+
+    const p = Plot.plot({
+        padding: 0,
+        aspectRatio: 1,
+        x: { axis: "top" },
+        color: computeColorOptions(),
+        marks: [
+            Plot.frame(),
+            Plot.cell(problems, { x: "a", y: "b", fill: heatmapView }),
+            Plot.text(problems, { x: "a", y: "b", text: (d) => computeText(d) }),
+            Plot.tip(problems, Plot.pointer({
+                x: "a", y: "b",
+                filter: (d) => d.timesSeen > 0,
+                channels: {
+                    correctPercent: (d) => d.correctPercent + "%",
+                    responseTime: (d) => d.responseTime + "s",
+                    timesSeen: "timesSeen",
+                },
+            })),
+        ]
+    })
+
+    p.addEventListener("input", (event) => {
+        if (p?.value?.a === undefined) return;
+        setHighlightedProblem(`${p.value.a}*${p.value.b}`);
+    });
+
+    return p
+}
+
+// time series plot of response time for a given problem
+export const responseTimeTsPlot = (stats, highlightedProblem) => {
+    const data = Object.values(stats.responseTimesPerProblem).flatMap(problem =>
+        problem.submissions.map(s => ({
+            name: problem.name, ...s, createdAt: new Date(s.createdAt), responseTimeS: s.responseTimeMs / 1000,
+        })))
+        .filter(it => it.isCorrect && it.name === highlightedProblem)
+        .toSorted((a, b) => a.createdAt < b.createdAt ? -1 : 1)
+
+    return Plot.plot({
+        title: `Response Time Trend For ${highlightedProblem}`,
+        y: { nice: true },
+        x: { nice: true },
+        marks: [
+            Plot.frame(),
+            Plot.dotY(data, {
+                x: "createdAt",
+                y: "responseTimeS",
+                fill: "name",
+            }),
+            Plot.lineY(data, Plot.windowY({ k: 2 }, { x: "createdAt", y: "responseTimeS", stroke: "median", curve: "auto" })),
+            Plot.tip(data, Plot.pointer({ x: "createdAt", y: "responseTimeS", channels: { problem: "name" } })),
+        ]
+    })
+}
---
+++ utils.js
@@ -0,0 +1,56 @@
+export function expect(condition, message) {
+    if (!condition) {
+        throw new Error(message);
+    }
+}
+
+export function withLRUCache(store, key, fn, maxSize = 100) {
+    // Ensure the key is a string, throw an error otherwise
+    // make sure the key is a string so iteration is via insertion order
+    if (typeof key !== 'string') {
+        throw new Error('withLRUCache: Key must be a string to ensure proper cache behavior.');
+    }
+
+    // if the key is already in the cache, move it to the "front"
+    if (store[key]) {
+        // kinda hacky since it relies on the fact that ecmascript's Object.keys
+        // returns an iterator based on insertion order for string keys
+        const value = store[key];
+        delete store[key];
+        store[key] = value;
+        return value;
+    }
+    const res = fn(); // cache miss, compute the value
+
+    // if the cache is full, remove the last item
+    const keys = Object.keys(store)
+    if (keys.length >= maxSize) {
+        const lastKey = keys[keys.length - 1];
+        delete store[lastKey];
+    }
+    store[key] = res;
+    return res;
+}
+
+const _appCache = window._appCache = {}
+export function withGlobalCache(key, fn) {
+    return withLRUCache(_appCache, key, fn, 1000)
+}
+
+export function formatMsAsS(ms) {
+    const res = (ms / 1000)
+    return Number.isNaN(res) ? undefined : res.toFixed(2) + "s";
+}
+
+export function evaluate(a, b, op) {
+    switch (op) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+    }
+}
